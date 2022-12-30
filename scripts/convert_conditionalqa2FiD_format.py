cd44aacffeb35b505facaaf6068216870627fbe4
Haritz Puerto <haritzpuerto94@gmail.com>
2022-12-30 15:47:29 +0100

    added question each truncated context

--- scripts/convert_conditionalqa2FiD_format.py
+++ scripts/convert_conditionalqa2FiD_format.py
@@ -59,16 +59,22 @@ def get_idx_lines(encodings, sent_sep_encoding):
     list_sent_idx.append((prev_idx, encodings.input_ids[0].tolist().index(1)))
     return list_sent_idx
 
-def truncate_section(tokenizer, section, max_length, sent_sep_encoding):
+def truncate_section(tokenizer, question, section, max_length, sent_sep_encoding):
     if len(section[1:]) > 0:
         ctx = " \\n ".join(section[1:])
-        txt = f"title: {section[0]} context: \\n {ctx}"
+        txt = f"{question} \\n title: {section[0]} context: \\n {ctx}"
     else:
         txt = f"title: {section[0]} context:"
 
     encodings = tokenizer(txt, return_tensors="pt", padding='max_length', truncation=True, max_length=max_length)
     list_sent_idx = get_idx_lines(encodings, sent_sep_encoding)
-    return tokenizer.decode(encodings.input_ids[0], skip_special_tokens=True), list_sent_idx
+    truncated_text = tokenizer.decode(encodings.input_ids[0], skip_special_tokens=True)
+    # remove \\n at the end 
+    # (in some edge cases, after the truncation, the last character is a \\n and 
+    # this would be considered as a sentence when creatin the graph)
+    if truncated_text[-2:] == "\\n":
+        truncated_text = truncated_text[:-2]
+    return truncated_text, list_sent_idx
 
 def format_data(tokenizer, data, url2doc, sent_sep_encoding, max_length: 512, print_info: False):
     list_len_sections = []
@@ -76,16 +82,17 @@ def format_data(tokenizer, data, url2doc, sent_sep_encoding, max_length: 512, pr
     dataset = []
     for x in tqdm(data):
         list_sections = get_sections(url2doc[x['url']]['contents'])
+        question = "Question: " + x['question'] + " Scenario: " + x['scenario']
         # truncate sections
         truncated_sec = []
         for s in list_sections:
-            truncated_sec_txt, list_sent_idx = truncate_section(tokenizer, s, max_length, sent_sep_encoding)
+            truncated_sec_txt, list_sent_idx = truncate_section(tokenizer, question, s, max_length, sent_sep_encoding)
             # truncated_sec_txt = [l.strip() for l in truncated_sec_txt.split("\\n")]
             truncated_sec.append({'title': s[0], 'text': truncated_sec_txt})
         # new data point
         new_x = {
                 'id': x['id'],
-                'question': "Question: " + x['question'] + " Scenario: " + x['scenario'],
+                'question': question,
                 'target': get_label(x),
                 'answers': [''],
                 'full_ctxs': [{'title': s[0], "text": s[1:]} for s in list_sections],
@@ -103,7 +110,7 @@ def format_data(tokenizer, data, url2doc, sent_sep_encoding, max_length: 512, pr
     
 if __name__ == "__main__":
     print(f"Loading tokenizer...")
-    tokenizer = T5Tokenizer.from_pretrained("t5-small", model_max_length=512)
+    tokenizer = T5Tokenizer.from_pretrained("t5-base", model_max_length=512)
     add_html_tags2tokenizer(tokenizer)
     sent_sep_encoding = tokenizer.get_added_vocab()['\\n']
     
@@ -119,7 +126,7 @@ if __name__ == "__main__":
         train = json.load(f)
     print(f"Format training data")
     training_dataset = format_data(tokenizer, train, url2doc, sent_sep_encoding, max_length=512, print_info=True)
-    with open("../data/fid_format/train.json", 'w') as f:
+    with open("../data/fid_format_base_qnode/train.json", 'w') as f:
         json.dump(training_dataset, f)
        
     print(f"Loading validation data from {training_path}") 
@@ -128,8 +135,8 @@ if __name__ == "__main__":
         dev = json.load(f)
     print(f"Format validation data")
     dev_dataset = format_data(tokenizer, dev, url2doc, sent_sep_encoding, max_length=512, print_info=True)
-    with open("../data/fid_format/dev.json", 'w') as f:
+    with open("../data/fid_format_base_qnode/dev.json", 'w') as f:
         json.dump(dev_dataset, f)
         
-    with open("../data/fid_format/DONE.txt", 'w') as f:
+    with open("../data/fid_format_base_qnode/DONE.txt", 'w') as f:
         f.write("DONE")
